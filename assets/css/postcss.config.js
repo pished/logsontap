c3e66d370654df022849cc806d444a6b8e845180
pished <goobuskus@gmail.com>
2020-02-22 14:18:09 -0500

    plaese work

--- assets/css/postcss.config.js
+++ assets/css/postcss.config.js
@@ -7,7 +7,21 @@ module.exports = {
             }), 
         require('tailwindcss')(themeDir + 'assets/css/tailwind.config.js'),
         // Configuration of purgecss for Tailwindcss
-        // see https://tailwindcss.com/docs/controlling-file-size/#setting-up-purgecss  
+        // see https://tailwindcss.com/docs/controlling-file-size/#setting-up-purgecss
+        require('@fullhuman/postcss-purgecss')({
+            // Specify the paths to all of the template files in your project 
+            content: [
+                themeDir + 'layouts/**/*.html',
+                themeDir + 'exampleSite/content/**/*.html',
+                'layouts/**/*.html',
+                'content/**/*.html',
+                'layouts/*.html',
+                'content/*.html',
+            ],
+            // Include any special characters you're using in this regular expression
+            defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [], 
+            fontFace: true
+        }),    
         require('autoprefixer')({
             grid: true
         }),
